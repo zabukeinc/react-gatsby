db4fd510abd0ccabae9f5b8d761a577145663356
zabukeinc <miftahregyana@gmail.com>
2020-06-04 19:14:15 +0700

    ganti akun git di windows

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>I'm building my own careers, also I have to improve my skillsets!</p>
+    <hr />
   </Layout>
 )
 
